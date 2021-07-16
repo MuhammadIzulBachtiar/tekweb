1c89525222568ae669a7ec10a84c19779753d460
Muhammad Izul Bachtiar 1900016126 <izulbahtiar1@gmail.com>
2021-07-16 20:38:34 +0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { ForgotComponent} from './auth/forgot/forgot.component';
+import { HomeComponent} from './auth/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'login',
+    component:LoginComponent
+  },
+  {
+    path: 'register',
+    component:RegisterComponent
+  },
+  {
+    path: 'forgot',
+    component:ForgotComponent
+  },
+  {
+    path: 'home',
+    component:HomeComponent
+  },
+  {
+    path:'admin',
+    loadChildren:()=>import('./admin/admin.module').then(mod=>mod.AdminModule)
+  },
+  {
+    path:'',
+    pathMatch:'full',
+    redirectTo:'/login'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
